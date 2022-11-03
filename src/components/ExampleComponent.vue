7e8bb8ee4f5686053f303019eb37625b8cf1d8a1
fr4nd3x <francisco-dg-1@hotmail.com>
2022-11-03 12:56:46 -0500

    event

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,35 +1,23 @@
 <template>
   <div>
-    <p>{{ title }}</p>
     <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
+      <li v-for="(todo, id) in msgs" :key="id" @click="increment">
+        {{ todo }}
       </li>
     </ul>
-    {{ msgs }}
-    <q-btn color="primary"  label="Activar alerta" @click="soss" />
-
-    <q-btn color="primary" icon="mail" label="DOCUMENTO" @click="doc" />
-
-    <q-btn color="primary" icon="mail" label="CONTACTO" @click="contact" />
-
-    {{ data.msg }}
-
     <q-btn
       v-for="(b, i) in buttons"
       :key="i"
       color="primary"
       icon="mail"
       :label="b"
-      @click="gere(b), ambi(b)"
+      @click="doc(b)"
     />
-
-
-
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
   </div>
+
+
+
+  
 </template>
 
 <script lang="ts">
@@ -46,18 +34,10 @@ function useClickCount() {
 
   return { clickCount, increment };
 }
-
 function useDisplayTodo(todos: Ref<Todo[]>) {
   const todoCount = computed(() => todos.value.length);
   return { todoCount };
 }
-
-function soss() {
-  axios.get('http://localhost:5000/1').then((r: any) => {
-    alert(r.data);
-  });
-}
-
 export default defineComponent({
   name: 'ExampleComponent',
   props: {
@@ -80,68 +60,31 @@ export default defineComponent({
 
   setup(props) {
     const buttons = ref([]);
-    const data = ref({ msg: '', ID: 2 });
+    const data = ref({ msg: '', ID: 1 }) ;
     const arr: any[] = [];
     const msgs = reactive(arr);
-    function contact(option: any) {
-      axios
-        .post('http://localhost:5000/contact', {
-          option: option.target ? option : '',
-          ID: data.value.ID,
-        })
-        .then((r: any) => {
-          msgs.push(['' + buttons.value, '' + data.value]);
-          buttons.value = r.data.options;
-          data.value = r.data;
-        });
-    }
-
-    function ambi(option: any) {
-      axios
-        .post('http://localhost:5000/ambi', {
-          option: option.target ? option : '',
-          ID: data.value.ID,
-        })
-        .then((r: any) => {
-          buttons.value = r.data.options;
-          //msgs.push(['' + buttons.value, '' + data.value]);
-          data.value = r.data;
-        });
-    }
-    function gere(option: any) {
-      axios
-        .post('http://localhost:5000/gere', {
-          option: option.target ? option : '',
-          ID: data.value.ID,
-        })
-        .then((r: any) => {
-          buttons.value = r.data.options;
-          //msgs.push(['' + buttons.value, '' + data.value]);
-          data.value = r.data;
-        });
-    }
 
     function doc(option: any) {
+      if (option) msgs.push(option);
       axios
         .post('http://localhost:5000/', {
-          option: option.target ? option : '',
+          option: !option.target ? option : '',
           ID: data.value.ID,
         })
         .then((r: any) => {
+          msgs.push(r.data.msg);
+
           buttons.value = r.data.options;
           data.value = r.data;
         });
     }
+    doc('');
 
     return {
+      doc,
       msgs,
       buttons,
       data,
-      ambi,
-      gere,
-      doc,
-      soss,
-      contact,
       ...useClickCount(),
       ...useDisplayTodo(toRef(props, 'todos')),
     };
